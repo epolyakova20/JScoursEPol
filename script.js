aa6b9e4bb76b9a6934a6214d5e15d1842a857655
Ekaterina Polyakova <epolyakova2020@mail.ru>
2026-09-02 22:33:46 +0300

    Задание урока lesson02 выполнено

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-let title
-let screens
-let screenPrice
-let rollback
-let fullPrice
-let adaptive
+let title = "Калькулятор стоимости сайтов";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 15000;
+let rollback = 10;
+let fullPrice = 90000;
+let adaptive = true;
 
-alert ("Первое сообщение по уроку");
-console.log("Сообщение в консоли");
+//использование методов и свойств
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+
+console.log("Стоимость верстки экранов ${screenPrice} руб");
+console.log("Стоимость разработки сайта ${fullPrice} руб");
+
+let screensLower = screens.toLowerCase();
+let screensArray = screensLower.split(", ");
+console.log("Массив типов экранов:", screensArray);
+
+console.log("Процент отката посреднику:", fullPrice * (rollback / 100));
