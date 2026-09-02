20b9c1ecf0c883fc6962ba47c28fc41e98c627fb
Ekaterina Polyakova <epolyakova2020@mail.ru>
2026-09-02 19:27:55 +0300

    добавила переменные в script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let title = "задание по JS"
-let screens
-let screenPrice
-let rollback
-let fullPrice
-let adaptive
+let title = "задание по JS";
+let screens;
+let screenPrice;
+let rollback;
+let fullPrice;
+let adaptive;
 
 alert (title)
