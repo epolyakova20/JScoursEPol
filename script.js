06a0acbf74e711b37df50424845dd368990915e5
Ekaterina Polyakova <epolyakova2020@mail.ru>
2026-09-08 21:51:36 +0300

    подготовка страницы выполнена

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const appData = {
-    title: "Калькулятор Верстки",
-    screens: "Шапка, Контент, Футер",
+    title: "",             
+    screens: "",           
+    screenPrice: 0,        
     adaptive: true,
     rollback: 10,
-    screenPrice: 0,        
+    service1: "",          
+    service2: "",          
     allServicePrices: 0,   
     fullPrice: 0,          
-    servicePercentPrice: 0,
-
+    servicePercentPrice: 0, 
+   
     getNumberInput: function(message) {
         let input;
         do {
@@ -21,19 +23,47 @@ const appData = {
         return Number(input);
     },
 
+    
+    getStringInput: function(message) {
+        let input;
+        do {
+            input = prompt(message);
+            if (input === null) {
+                input = "";
+            }
+            input = input.trim();
+        } while (input === "" || !isNaN(Number(input)));
+        
+        return input;
+    },
+  
     getTitle: function() {
-    const trimmedTitle = this.title.trim(); 
-    if (!trimmedTitle) return "";
-    this.title = trimmedTitle[0].toUpperCase() + trimmedTitle.slice(1).toLowerCase();
-},
+        const trimmedTitle = this.title.trim(); 
+        if (!trimmedTitle) return;
+        this.title = trimmedTitle[0].toUpperCase() + trimmedTitle.slice(1).toLowerCase();
+    },
 
+    
     asking: function() {
-        this.screenPrice = this.getNumberInput("Сколько будет стоить верстка проекта?");
+        this.title = this.getStringInput("Как называется ваш проект?");
+        
+        this.getTitle();
+
+        this.screens = this.getStringInput("Какие типы экранов нужно разработать?");
+
+        this.screenPrice = this.getNumberInput("Сколько будет стоить данная работа?");
+        
+        this.adaptive = confirm("Нужен ли адаптив на сайте?");
     },
 
     getAllServicePrices: function() {
-        const servicePrice1 = this.getNumberInput("Первая дополнительная услуга: Сколько это будет стоить?");
-        const servicePrice2 = this.getNumberInput("Вторая дополнительная услуга: Сколько это будет стоить?");
+        this.service1 = this.getStringInput("Какой первый дополнительный тип услуги нужен?");
+        
+        const servicePrice1 = this.getNumberInput("Сколько это будет стоить?");
+
+        this.service2 = this.getStringInput("Какой второй дополнительный тип услуги нужен?");
+        
+        const servicePrice2 = this.getNumberInput("Сколько это будет стоить?");
         
         this.allServicePrices = servicePrice1 + servicePrice2;
     },
@@ -55,7 +85,7 @@ const appData = {
 
     logger: function() {
         console.log("--- НЕОБХОДИМАЯ ИНФОРМАЦИЯ ---");
-        console.log("Тип данных title:", typeof this.title);
+        console.log("Название проекта (title):", this.title);
         console.log("Тип данных fullPrice:", typeof this.fullPrice);
         console.log("Тип данных adaptive:", typeof this.adaptive);
         console.log("Экраны для разработки:", this.screens);
@@ -69,16 +99,12 @@ const appData = {
     },
 
     start: function() {
-        this.getTitle();
         this.asking();                      
         this.getAllServicePrices();         
         this.getFullPrice();                
         this.getServicePercentPrices();     
-        
         this.logger();                      
     }
 };
 
-
 appData.start();
-
